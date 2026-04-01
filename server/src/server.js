8bf7e27b3ed79d920a1cc18bb69d2e4f1ca55114
Avery <averymiller2060@gmail.com>
2026-03-31 21:04:03 -0400

    added api routes and moved db connection to its own directory

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import 'dotenv/config';
+import db from './config/connection.js';
+import { userRoutes, noteRoutes } from './routes/index.js';
 
-dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
-
 app.get('/', (req, res) => res.send('Tag-it Notes backend running'));
 
+app.use('/api/user', userRoutes);
+app.use('/api/note', noteRoutes);
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`[CONNECTED] Server running on port ${PORT}`);
+  });
+});
