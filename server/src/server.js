628d4073938a38cf879fcf1ccd8517877e9c5b63
Avery <averymiller2060@gmail.com>
2026-04-01 20:08:11 -0400

    added middleware for debugging purposes

--- server/src/server.js
+++ server/src/server.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Middleware logging
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.get('/', (req, res) => res.send('Tag-it Notes backend running'));
 
 app.use('/api/user', userRoutes);
@@ -17,7 +23,12 @@ app.use('/api/note', noteRoutes);
 const PORT = process.env.PORT || 5000;
 
 db.once('open', () => {
+  console.log(`[CONNECTED] DB connection established`)
   app.listen(PORT, () => {
     console.log(`[CONNECTED] Server running on port ${PORT}`);
   });
+});
+
+db.on('error', (err) => {
+  console.error('[ERROR] MongoDB error:', err);
 });
